3e781b600148a590ab28333a6bb56b400a02d56e
hisreal123 <omosgoerge@gmail.com>
2024-12-08 20:18:33 +0100

    fix task 0

--- utils/db.js
+++ utils/db.js
@@ -1,66 +1,42 @@
-import mongodb from 'mongodb';
-// eslint-disable-next-line no-unused-vars
-import Collection from 'mongodb/lib/collection';
-import envLoader from './env_loader';
+import "dotenv/config";
+import { MongoClient } from "mongodb";
 
-/**
- * Represents a MongoDB client.
- */
+
 class DBClient {
-  /**
-   * Creates a new DBClient instance.
-   */
   constructor() {
-    envLoader();
-    const host = process.env.DB_HOST || 'localhost';
+    const host = process.env.DB_HOST || "localhost";
     const port = process.env.DB_PORT || 27017;
-    const database = process.env.DB_DATABASE || 'files_manager';
-    const dbURL = `mongodb://${host}:${port}/${database}`;
+    const database = process.env.DB_DATABASE || "files_manager";
+    const dbURL = process.env.DB_URL;
 
-    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
-    this.client.connect();
+    this.isConnecedToDB = false;
+
+    this.client = new MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client.connect()
+      .then(() => this.isConnecedToDB = true)
+      .catch(() => this.isConnecedToDB = false);
   }
 
-  /**
-   * Checks if this client's connection to the MongoDB server is active.
-   * @returns {boolean}
-   */
-  isAlive() {
-    return this.client.isConnected();
-  }
+  isAlive = () => {
+    return this.isConnecedToDB;
+  };
 
-  /**
-   * Retrieves the number of users in the database.
-   * @returns {Promise<Number>}
-   */
   async nbUsers() {
-    return this.client.db().collection('users').countDocuments();
+    return this.client.db().collection("users").countDocuments();
   }
 
-  /**
-   * Retrieves the number of files in the database.
-   * @returns {Promise<Number>}
-   */
   async nbFiles() {
-    return this.client.db().collection('files').countDocuments();
+    return this.client.db().collection("files").countDocuments();
   }
 
-  /**
-   * Retrieves a reference to the `users` collection.
-   * @returns {Promise<Collection>}
-   */
-  async usersCollection() {
-    return this.client.db().collection('users');
+  async userCollection() {
+    return this.client.db().collection("users");
   }
 
-  /**
-   * Retrieves a reference to the `files` collection.
-   * @returns {Promise<Collection>}
-   */
   async filesCollection() {
-    return this.client.db().collection('files');
+    return this.client.db().collection("files");
   }
 }
 
-export const dbClient = new DBClient();
+const dbClient = new DBClient();
 export default dbClient;
--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
