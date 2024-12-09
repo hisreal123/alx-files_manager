2caebf606d52f08be5f9f9b979602dfc38575f8f
hisreal123 <omosgoerge@gmail.com>
2024-12-09 23:39:33 +0100

    mongodb headache

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+// import { Express } from 'express';
+import AppController from "../controllers/AppController";
+
+const Routes = (api) => {
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats)
+}
+
+export default Routes;
+
--- server.js
+++ server.js
@@ -1,4 +1,13 @@
-import { express } from 'express'
+import 'dotenv/config';
+import express from 'express';
+import Routes from './routes';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+Routes(app);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
